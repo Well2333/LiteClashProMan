240d9167f5e6e03b68ff803c61f182b9c204ed6f
Well404 <well_404@outlook.com>
2023-06-07 01:28:33 +0800

    :fire: 移除过时的验证器

--- LiteClashProMan/model/clash/proxygroup.py
+++ LiteClashProMan/model/clash/proxygroup.py
@@ -1,6 +1,6 @@
 from typing import Literal, Optional, Union, List
 
-from pydantic import BaseModel, Extra, validator
+from pydantic import BaseModel, Extra
 
 
 class ProxyGroupTemplate(BaseModel, extra=Extra.allow):
@@ -10,16 +10,6 @@ class ProxyGroupTemplate(BaseModel, extra=Extra.allow):
     url: Optional[str]
     interval: Optional[int]
     _index: Optional[int]
-
-    @validator("url", "interval")
-    def check_type(cls, v, values):
-        if values["type"] == "url-test" and v is None:
-            raise ValueError('When type is "url-test", url and interval cannot be None')
-        elif values["type"] != "url-test" and v != None:
-            raise ValueError(
-                'When type is not "url-test", url and interval must be None'
-            )
-        return v
 
 
 class ProxyGroup(ProxyGroupTemplate):
